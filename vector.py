efd750242b364617a2b3c9af1a3a63484bdbe42f
DojoZheng <zhengdongjia@foxmail.com>
2017-12-06 21:26:38 +0800

    Inner product & angle calculation

--- vector.py
+++ vector.py
@@ -1,4 +1,7 @@
-from math import sqrt
+from math import sqrt, acos, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
 
 class Vector(object):
 
@@ -6,8 +9,8 @@ class Vector(object):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
-            self.dimension = len(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
+            self.dimension = len(self.coordinates)
 
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
@@ -37,7 +40,7 @@ class Vector(object):
         return Vector(new_coordinates)
 
     def times_scalar(self, c):
-        new_coordinates = [c*x for x in self.coordinates]
+        new_coordinates = [Decimal(c)*x for x in self.coordinates]
         return Vector(new_coordinates)
 
     # define Magnitude & Direction
@@ -47,11 +50,32 @@ class Vector(object):
 
     def normalized(self):
     	try:
+    		# We have to use Decimal in order to avoid boundary error
     		magnitude = self.magnitude()
-    		return self.times_scalar(1./magnitude)
-
+    		return self.times_scalar(Decimal('1.0')/Decimal(magnitude))
     	except ZeroDivisionError:
-    		raise Exception("Cannot normalize the zero Vector")
+    		raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+
+    def dot(self, v):
+    	return sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
+
+    def angle_with(self, v, in_degrees = False):
+    	try:
+    		u1 = self.normalized()
+    		u2 = v.normalized()
+    		angle_in_radians = acos(u1.dot(u2))
+
+    		if in_degrees:
+    			degrees_per_radian = 180. / pi
+    			return angle_in_radians * degrees_per_radian
+    		else:
+    			return angle_in_radians
+
+    	except Exception as e:
+    		if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+    			raise Exception("Cannot compute an angle with the zero vector")
+    	else:
+    		raise e
 
 # 1. test
 my_vector = Vector([1, 2, 3])
@@ -62,6 +86,7 @@ my_vector3 = my_vector2
 print my_vector2 == my_vector3
 print my_vector3 == my_vector
 print my_vector2 == my_vector
+print('\n')
 
 # 2. Operating
 v = Vector([8.218, -9.341])
@@ -69,12 +94,25 @@ w = Vector([-1.129, 2.111])
 print v.plus(w)
 print v.minus(w)
 print v.times_scalar(2)
+print('\n')
 
 # 3. Magnitude & Direction
-v = Vector([-0.221, 7.437])
+v = Vector(['-0.221', '7.437'])
 print ("Magnitude(-0.221, 7.437) = " + str(v.magnitude()))
 
-v = Vector([5.581, -2.136])
+v = Vector(['5.581', '-2.136'])
 print ("Normalized([5.581, -2.136]) = " + str(v.normalized()))
+print('\n')
+
+# 4. Inner/ Dot Product & Angle
+v = Vector(['7.887', '4.138'])
+w = Vector(['-8.802', '6.776'])
+print("v.w = " + str(v.dot(w)))
+
+v = Vector(['3.183', '-7.627'])
+w = Vector(['-2.668', '5.319'])
+print ("v angle with w:" + str(v.angle_with(w)))
+
+print('\n')
 
 
