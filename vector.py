e137e9e3800501dc5fcd11b326ddc6da109f60e5
DojoZheng <zhengdongjia@foxmail.com>
2017-12-09 18:32:05 +0800

    Parallel Component & Orthogonal Component

--- vector.py
+++ vector.py
@@ -1,7 +1,7 @@
 from math import sqrt, acos, pi
 from decimal import Decimal, getcontext
 
-getcontext().prec = 30
+getcontext().prec = 10
 
 class Vector(object):
 
@@ -90,6 +90,29 @@ class Vector(object):
                 abs(self.angle_with(v) - 0) < tolerance or
                 abs(self.angle_with(v) - pi) < tolerance)
 
+    def component_parallel_to(self, basis):
+    	try:
+    		u = basis.normalized()
+    		weight = self.dot(u)
+    		return u.times_scalar(weight)
+    	except Exception as e:
+    		if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+    			# There is no unique parallel component to the Zero Vector
+    			raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+    	else:
+    		raise e
+
+    def component_orthogonal_to(self, basis):
+    	try:
+    		projection = self.component_parallel_to(basis)
+    		return self.minus(projection)
+    	except Exception as e:
+    		if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+    			raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+    		else:
+    			raise e
+
+
 # 1. test
 my_vector = Vector([1, 2, 3])
 print my_vector
@@ -137,3 +160,28 @@ print w
 print 'is parallel :', v.is_parallel_to(w)
 print 'is orthogonal :', v.is_orthogonal_to(w)
 
+# 6. Parallel Component & Orthogonal Component
+print ('\nParallel Component & Orthogonal Component')
+v = Vector([3.039, 1.879])
+w = Vector([0.825, 2.036])
+print '#1'
+# print v 
+# print w
+print v.component_parallel_to(w)
+
+print '\n#2'
+v = Vector([-9.88, -3.264, -8.159])
+w = Vector([-2.155, -9.353, -9.473])
+# print v 
+# print w
+print v. component_orthogonal_to(w)
+
+print '\n#3'
+v = Vector([3.009, -6.172, 3.692, -2.51])
+w = Vector([6.404, -9.144, 2.759, 8.718])
+# print v 
+# print w
+vpar = v.component_parallel_to(w)
+vort = v.component_orthogonal_to(w)
+print 'parallel component:', vpar
+print 'orthogonal component:', vort
