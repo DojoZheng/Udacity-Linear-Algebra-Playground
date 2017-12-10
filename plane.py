5eb9dcc6879aa05ee4ed61d513897b4445b53fb9
DojoZheng <zhengdongjia@foxmail.com>
2017-12-10 20:34:21 +0800

    Plane's parallel and equal function

--- plane.py
+++ plane.py
@@ -88,6 +88,30 @@ class Plane(object):
 
         return output
 
+    def __eq__(self, p):
+        if self.normal_vector.is_zero():
+            if not p.normal_vector.is_zero():
+                return False
+            else:
+                diff = self.constant_term - p.constant_term
+                return MyDecimal(diff).is_near_zero()
+        elif p.normal_vector.is_zero():
+            return False
+
+        if not self.is_parallel_to(p):
+            return False
+
+        x0 = self.basepoint
+        y0 = p.basepoint
+        basepoint_difference = x0.minus(y0)
+
+        n = self.normal_vector
+        return basepoint_difference.is_orthogonal_to(n)
+
+    def is_parallel_to(self, p):
+        n1 = self.normal_vector
+        n2 = p.normal_vector
+        return n1.is_parallel_to(n2)
 
     @staticmethod
     def first_nonzero_index(iterable):
@@ -100,3 +124,20 @@ class Plane(object):
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
+
+
+# Test
+p1 = Plane(normal_vector=Vector(['-0.412', '3.806', '0.728']), constant_term='-3.46')
+p2 = Plane(normal_vector=Vector(['1.03', '-9.515', '-1.82']), constant_term='8.65')
+print 'first pair of planes are parallel?:{}'.format(p1.is_parallel_to(p2))
+print 'first pair of planes are equal?:{}'.format(p1 == p2)
+
+p1 = Plane(normal_vector=Vector(['2.611', '5.528', '0.283']), constant_term='4.6')
+p2 = Plane(normal_vector=Vector(['7.715', '8.306', '5.342']), constant_term='3.76')
+print 'second pair of planes are parallel?:{}'.format(p1.is_parallel_to(p2))
+print 'second pair of planes are equal?:{}'.format(p1 == p2)
+
+p1 = Plane(normal_vector=Vector(['-7.926', '8.625', '-7.212']), constant_term='-7.952')
+p2 = Plane(normal_vector=Vector(['-2.642', '2.875', '-2.404']), constant_term='-2.443')
+print 'third pair of planes are parallel?:{}'.format(p1.is_parallel_to(p2))
+print 'third pair of planes are equal?:{}'.format(p1 == p2)
